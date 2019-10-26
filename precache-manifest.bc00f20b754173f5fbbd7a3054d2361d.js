ee1051074aedacc059e8e35df2c89368ab73937d
AiJey <aijeykob@gmail.com>
2019-10-26 15:26:21 +0300

    Updates

--- precache-manifest.bc00f20b754173f5fbbd7a3054d2361d.js
+++ precache-manifest.bc00f20b754173f5fbbd7a3054d2361d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "89f6e5aee2d425b5e6655fdab1751720",
+    "revision": "57eebea30774ab1921e2eb89780e68df",
     "url": "/lavier/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lavier/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "a35c7c67dd53ae25c7f3",
+    "revision": "8e62f41a7803c1b86ada",
     "url": "/lavier/static/css/main.0aeb3b79.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lavier/static/js/2.35619288.chunk.js"
   },
   {
-    "revision": "a35c7c67dd53ae25c7f3",
-    "url": "/lavier/static/js/main.79c2e0ab.chunk.js"
+    "revision": "8e62f41a7803c1b86ada",
+    "url": "/lavier/static/js/main.62fc1f8f.chunk.js"
   },
   {
     "revision": "f896c134be4f6345dbb3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lavier/precache-manifest.1851f50b3885a5a174f6ed48f1e2f1bf.js"
+  "/lavier/precache-manifest.bc00f20b754173f5fbbd7a3054d2361d.js"
 );
 
 self.addEventListener('message', (event) => {
