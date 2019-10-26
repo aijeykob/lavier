2dfb5cd7775415fac18ffc7b080f3be9937bda88
AiJey <aijeykob@gmail.com>
2019-10-26 15:05:03 +0300

    Updates

---
+++ precache-manifest.30e1180ab686733c66cc8824dcb9e4a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7afd36574925ba9ad60fa161f765d7",
+    "url": "/lavier/index.html"
+  },
+  {
+    "revision": "8ef86b46e70c7d83a16d",
+    "url": "/lavier/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "8e4d998e4afd9163b9a1",
+    "url": "/lavier/static/css/main.0aeb3b79.chunk.css"
+  },
+  {
+    "revision": "8ef86b46e70c7d83a16d",
+    "url": "/lavier/static/js/2.35619288.chunk.js"
+  },
+  {
+    "revision": "8e4d998e4afd9163b9a1",
+    "url": "/lavier/static/js/main.e3650953.chunk.js"
+  },
+  {
+    "revision": "f896c134be4f6345dbb3",
+    "url": "/lavier/static/js/runtime~main.9eb7d21b.js"
+  }
+]);
